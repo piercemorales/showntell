784da13bd8cef70b7aa3ed8a7b2cad021e33a39d
Pierce <nathanm0220@gmail.com>
2019-12-06 16:15:50 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,34 @@
+$('.tilt').tilt({
+
+})
+
+$('.tilt2').tilt({
+  scale: 0.6
+})
+
+$('.tilt3').tilt({
+  scale: 1.3
+})
+
+$('.tilt4').tilt({
+  glare: true,
+  scale: 1.2
+  
+})
+
+$('.tilt5').tilt({
+  reset: false
+
+})
+
+$('.tilt6').tilt({
+  scale: 1.2,
+  glare: true,
+
+})
+
+$('.tilt8').tilt({
+  scale: 1.2,
+  glare: true,
+
+})
